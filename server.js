47cfd2a637ae3d00bffa7fe78570c82e607609fa
Alexar <github@alexar.me>
2025-06-11 19:15:06 +1000

    Potential fix

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Fallback to index.html (for SPA support if needed)
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
 // Start server
